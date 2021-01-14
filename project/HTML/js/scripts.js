1e075336d48860ebd5b13d7bcfa1f46a3251f9f2
MohamedBenHammadi <71707362+MohamedBenHammadi@users.noreply.github.com>
2021-01-14 12:28:47 +0100

    initial commit

--- project/HTML/js/scripts.js
+++ project/HTML/js/scripts.js
@@ -4,5 +4,24 @@ $(document).ready(function(){
       slidesToShow: 1,
       autoplay: true,
       autoplaySpeed: 3200,
+      arrows: false,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+        {
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 1,
+          
+          }
+        }
+      ]
     });
   });
